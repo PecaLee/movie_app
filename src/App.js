ad88e97b062b919863cd35e4afb77e9ad6fdc306
pecalee <studiopeca.lee@gmail.com>
2021-02-06 20:30:09 +0900

    PropTypes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,74 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({fav}) {
-  return <h2>I Like {fav}!</h2>
+function Food({ name, picture, rating }) {
+  //prop.fav로 인자를 넣는거랑 {fav}로 넣는거랑 같은 결과를 가져온다
+  return (
+    <div>
+      <h2>I Like {name}!</h2>
+      <img src={picture} alt={name} />
+      <h4>rating : {rating}</h4>
+    </div>
+  );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 4.9,
+  },
+  {
+    id: 2,
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 4,
+  },
+  {
+    id: 3,
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 3.3,
+  },
+  {
+    id: 4,
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 2,
+  },
+  {
+    id: 5,
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 4.5,
+  },
+];
 
 function App() {
   return (
-  <div>
-    <h1>Hello</h1>
-    <Food fav="kimchi" />
-    <Food fav="samgyetang" />
-    <Food fav="ramen" />
-    <Food fav="kimpap" />
-    <Food fav="popcorn" />
-  </div>
+    <div>
+      <h1>Hello</h1>
+      {foodILike.map((dish) => (
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
+      ))}
+    </div>
   );
 }
 
