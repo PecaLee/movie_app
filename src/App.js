494ae3fb1a4f5dcceb7dacf14ccc1e132c4079d1
pecalee <studiopeca.lee@gmail.com>
2021-02-06 17:12:36 +0900

    how to use prop

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from "react";
+
+function Food({fav}) {
+  return <h2>I Like {fav}!</h2>
+}
+
 function App() {
   return (
   <div>
     <h1>Hello</h1>
-  </div>);
+    <Food fav="kimchi" />
+    <Food fav="samgyetang" />
+    <Food fav="ramen" />
+    <Food fav="kimpap" />
+    <Food fav="popcorn" />
+  </div>
+  );
 }
 
 export default App;
