c4a1bfd2133e90fa3ea035bf273bdb92faafa7df
pecalee <studiopeca.lee@gmail.com>
2021-02-09 22:57:04 +0900

    router - Link, HashRouter, BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 import About from "./routes/About";
 import Home from "./routes/Home";
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Route path="/" exact={true}>
         <Home />
       </Route>
